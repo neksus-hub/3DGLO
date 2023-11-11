79339ad549dca4a9713bdacc044d0d399f324751
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-11-11 22:20:10 +0300

    added scroll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import scroll from "./modules/scroll";
 
 timer("15 november 2023");
 menu();
 modal();
+scroll();
---
+++ src/modules/scroll.js
@@ -0,0 +1,3 @@
+const scroll = () => {};
+
+export default scroll;
