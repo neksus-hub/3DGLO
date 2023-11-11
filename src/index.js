d78dfb924c1bf7b6419e91b050eb674d146df039
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-11-11 21:50:34 +0300

    added modal window animation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 
-timer("10 november 2023");
+timer("15 november 2023");
 menu();
 modal();
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,16 +2,46 @@ const modal = () => {
   const modal = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
   const popupClose = document.querySelector(".popup-close");
+  const modalContent = document.querySelector(".popup-content");
 
-  buttons.forEach((btn) =>
-    btn.addEventListener("click", () => {
-      modal.style.display = `block`;
-    })
-  );
+  let width = document.documentElement.clientWidth;
+
+  let idInterval;
+  let count = 0;
+
+  const openPopup = () => {
+    count++;
+    idInterval = requestAnimationFrame(openPopup);
+
+    if (count <= 23) {
+      modalContent.style.top = count * 0.8 + "%";
+      console.log(count);
+    } else {
+      cancelAnimationFrame(idInterval);
+    }
+  };
+
+  const listener = () => {
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        width = document.documentElement.clientWidth;
+        modal.style.display = `block`;
+        if (width > 768) {
+          console.log(",jkmit");
+          openPopup();
+        }
+      });
+    });
+  };
 
   popupClose.addEventListener("click", () => {
     modal.style.display = `none`;
+    modalContent.style.top = "";
+    count = 0;
+    listener();
   });
+
+  listener();
 };
 
 export default modal;
